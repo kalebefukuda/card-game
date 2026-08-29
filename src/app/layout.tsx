0c08e20cc230612cdef029b8349b863ff685f07c
kalebefukuda <kalebefukudadeoliveira@gmail.com>
2026-08-29 13:59:12 -0300

    chore: set document title and locale

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,17 +1,17 @@
-import "./globals.css";
+import type { Metadata } from 'next'
+import './globals.css'
+
+export const metadata: Metadata = {
+  title: 'Cards Just Cards',
+  description: 'O jogo de cartas mais sem noção para jogar com os amigos.',
+}
 
 export default function RootLayout({
   children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <html lang="en">
-      <body
-        className='antialiased'
-      >
-        {children}
-      </body>
+    <html lang="pt-BR">
+      <body className="antialiased">{children}</body>
     </html>
-  );
+  )
 }
